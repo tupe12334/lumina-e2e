19623013bf01fa9a5810b24f7956dd0629b13fe7
tupe12334 <ofek.gabay.he@gmail.com>
2025-08-12 15:01:44 +0300

    fix: update Playwright configuration for correct baseURL and port

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
-  testDir: './playwright',
-  reporter: 'html',
+  testDir: "./playwright",
+  reporter: "html",
   use: {
-    baseURL: 'http://localhost:4173',
-    screenshot: 'on',
-    trace: 'on',
+    baseURL: "http://localhost:5766",
+    screenshot: "on",
+    trace: "on",
     viewport: { width: 1280, height: 720 },
   },
   webServer: {
-    command: 'pnpm --dir ../client preview',
-    port: 4173,
+    command: "pnpm --dir ../client preview",
+    port: 5766,
     reuseExistingServer: !process.env.CI,
   },
 });
